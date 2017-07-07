7a4aca29feb2c83316f53b78b21701e61d496184
alejandro0627 <Alejo.giraldo1999@gmail.com>
2017-07-07 13:11:49 -0400

    Create basic_data_types.cpp

---
+++ basic_data_types.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main() {
+    int d;
+    long ld;
+    char c;
+    float f;
+    double lf;
+  
+  scanf("%d %ld %c %f %lf" ,&d,&ld,&c,&f,&lf);
+  printf("%d\n%ld\n%c\n%.3f\n%.9lf\n",d,ld,c,f,lf);
+ 
+    return 0;
+}
